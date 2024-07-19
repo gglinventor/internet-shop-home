22e042672a1048e6dae9890b529762c00a935d10
GGLinventor <example@mail.com>
2024-07-19 20:37:58 +0300

    Second pagination commit

--- goods/views.py
+++ goods/views.py
@@ -3,14 +3,16 @@ from django.shortcuts import get_list_or_404, render
 from goods.models import Products
 
 
-def catalog(request, category_slug, page=1):
+def catalog(request, category_slug):
+    page = request.GET.get('page', 1)
+    
     if category_slug == 'all':
         goods = Products.objects.all()
     else:
         goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
 
     paginator = Paginator(goods, 3)
-    current_page = paginator.page(page) #текущая страница    
+    current_page = paginator.page(int(page)) #текущая страница    
 
     context = {
         "title": "Home Каталог",
