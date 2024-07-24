a32bcb410ed265f3e67564cfbd71abba882fda2d
GGLinventor <example@mail.com>
2024-07-24 14:47:29 +0300

    Simple search commit

--- goods/views.py
+++ goods/views.py
@@ -1,18 +1,22 @@
 from django.core.paginator import Paginator
 from django.shortcuts import get_list_or_404, render
 from goods.models import Products
+from goods.utils import q_search
 
 
-def catalog(request, category_slug):
+def catalog(request, category_slug=None):
     page = request.GET.get('page', 1)
     on_sale = request.GET.get('on_sale', None)
     order_by = request.GET.get('order_by', None)
+    query = request.GET.get('q', None)
     
     
     if category_slug == 'all':
         goods = Products.objects.all()
     #else:
         #goods = get_list_or_404(Products.objects.filter(category__slug=category_slug)) не работает. Ниже код, чтобы хоть как-то отображалось
+    elif query:
+        goods = q_search(query)
     else:
         goods = Products.objects.filter(category__slug=category_slug)
 
@@ -36,6 +40,7 @@ def product(request, product_slug):
     product = Products.objects.get(slug=product_slug)
 
     context = {
+        "title": f"Home {product.name}",
         "product": product,
     }
 
