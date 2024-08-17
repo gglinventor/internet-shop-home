0d47e0264cb32bb3c2dd29f8754ab4b1ae635a45
GGLinventor <example@mail.com>
2024-08-17 14:26:07 +0300

    Remove from cart ajax commit

--- carts/views.py
+++ carts/views.py
@@ -37,7 +37,23 @@ def cart_add(request):
 def cart_change(request, product_slug):
     ...
 
-def cart_remove(request, cart_id):
+def cart_remove(request):
+    cart_id = request.POST.get('cart_id')
     cart = Cart.objects.get(id=cart_id)
+    
+    quantity = cart.quantity
     cart.delete()
-    return redirect(request.META['HTTP_REFERER']) #"request.META['HTTP_REFERER']" - с какой страницы попал в этот контролер
+    
+    user_cart = get_user_carts(request)
+    print(user_cart)
+    
+    cart_items_html = render_to_string(
+        "carts/includes/included_cart.html", {"carts": user_cart}, request=request) #новая прорисовка корзины
+
+    response_data = {
+        "message": 'Товар удалён',
+        "quantity_deleted": quantity,
+        "cart_items_html": cart_items_html,
+    }
+    
+    return JsonResponse(response_data)
